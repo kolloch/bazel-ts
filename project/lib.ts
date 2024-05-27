21ef8ed323fbe5d98e3583578b4ddd6fc990920e
Peter Kolloch <peter.kolloch@nexxiot.com>
2024-05-27 15:45:31 +0200

    Sample with generated file

--- project/lib.ts
+++ project/lib.ts
@@ -1,5 +1,6 @@
 import { other } from "./other";
+import { uniqueStringName } from "./generated";
 
 export function hello() {
-    return "Hello, world!" + other()
+    return "Hello, world!" + other() + uniqueStringName
 }
